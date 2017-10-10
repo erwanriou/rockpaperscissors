9646033bf1eb6057001ed6b8c9c5533e3bdf1ffc
Erwan Riou <riou@viamundus.com>
2017-10-10 17:29:28 +0200

    validate 23 run test

--- js/game-logic.js
+++ js/game-logic.js
@@ -21,5 +21,27 @@ let playerTwoMoveThreeValue;
 
 // Let's set the player move function here...
 let setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
+  if (player === "player One") {
+    let moveOneType = "rock";
+  }
+
+  if (player === "player Two") {
+    let moveOneType = "rock";
+  }
+};
+
+// Let's set the round winner function here...
+let getRoundWinner = () => {
+
+};
+
+
+// Let's set the computer moves function here...
+let setComputerMoves = () => {
+
+};
+
+// Let's set the computer moves function here...
+let getGameWinner = () => {
 
 };
