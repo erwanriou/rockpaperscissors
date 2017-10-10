47d8e8f98f9955447a3d5e2350667e2f8e2d31b4
Erwan Riou <riou@viamundus.com>
2017-10-10 16:46:42 +0200

    validate 13 run test

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,2 +1,25 @@
 // All code should be written in this file.
-let playerOneMoveOneType = "";
+
+// Player One variables
+let playerOneMoveOneType;
+let playerOneMoveTwoType;
+let playerOneMoveThreeType;
+
+let playerOneMoveOneValue;
+let playerOneMoveTwoValue;
+let playerOneMoveThreeValue;
+
+
+// Player Two variables
+let playerTwoMoveOneType;
+let playerTwoMoveTwoType;
+let playerTwoMoveThreeType;
+
+let playerTwoMoveOneValue;
+let playerTwoMoveTwoValue;
+let playerTwoMoveThreeValue;
+
+// Let's set the player move function here...
+let setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
+
+};
